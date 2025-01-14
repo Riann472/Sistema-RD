e71327c36759bcea1ec1cb98137a17391cff8429
Shedy <rianncarvalhomota472@gmail.com>
2025-01-14 17:25:17 -0300

    Terminada a home, login ainda incompleto.

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,7 +1,16 @@
-export default function Card(){
-    return (
-        <div>
+import Caixa from '../../assets/caixa.png'
+import { Link } from 'react-router-dom'
+import styles from './Card.module.css'
 
-        </div>
+export default function Card({ title, img, link }) {
+    return (
+
+        <Link to={link}>
+            <div className={styles.card}>
+                <h1>{title}</h1>
+                <img src={img} alt={title} />
+            </div>
+        </Link>
+
     )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,16 @@
 import Card from '../layout/Card'
+import Caixa from '../../assets/caixa.png'
+import Produtos from '../../assets/produtos.svg'
+import Clientes from '../../assets/clientes.png'
+
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <section>
-            <Card></Card>
+        <section className={styles.home}>
+            <Card title="PDV" img={Caixa} link="/pdv"></Card>
+            <Card title="Produtos" img={Produtos} link="/produtos"></Card>
+            <Card title="Clientes" img={Clientes} link="/cliente"></Card>  
         </section>
     )
 }
